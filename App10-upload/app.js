5771a1cee926a92da906137b6375443a4cd8c861
txBoy <tx@vimplus.com>
2017-10-18 17:20:42 +0800

    test sender.js

--- App10-upload/app.js
+++ App10-upload/app.js
@@ -3,6 +3,8 @@ const koa = require('koa');
 const koaBody = require('koa-body');
 const Router = require('koa-router');
 const staticServer = require('koa-static');
+const fs = require('fs');
+const path = require('path');
 const uploadService = require('./uploadService');
 
 const app = new koa();
@@ -16,6 +18,8 @@ app.use(router.routes()).use(router.allowedMethods());
 
 router.post('/api/files/upload', async (ctx, next) => {
     var content = ctx.request.body;
+    console.log('----content:', content)
+    // uploadFiles 是input中的name属性值
     var files = content && content.files && content.files.uploadFiles || null;
     if (!files) {
         ctx.body = {
@@ -42,6 +46,37 @@ router.post('/api/files/upload', async (ctx, next) => {
         }
     }
 
+})
+
+
+router.post('/api/sender/upload', async (ctx, next) => {
+    var content = ctx.request.body;
+    var file = content && content.file || null;
+    console.log('----------file:', file)
+    if (!file) {
+        ctx.body = {
+            code: 99999,
+            data: null,
+            msg: 'error'
+        }
+        return;
+    } else {
+        let readFrom = fs.createReadStream(file.path);
+        let saveTo = fs.createWriteStream(path.join(__dirname + '/upload/test.png'));
+        readFrom.pipe(saveTo);
+        readFrom.on('end', function (ret) {
+            console.log('-----------ret:', ret)
+        });
+
+        readFrom.on('error', function (err) {
+            console.log('-----------err:', err)
+        });
+        ctx.body = {
+            code: 10000,
+            data: '上传成功！',
+            msg: 'success'
+        }
+    }
 })
 
 app.listen(port, () => {
